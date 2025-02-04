576a85737b447b5ce765126abd1c0f51d2e65f11
zhafran2 <zhafranm.irsyad@gmail.com>
2025-02-04 14:41:51 +0700

    feat:set up awal

---
+++ server/app.js
@@ -0,0 +1,15 @@
+const express =require('express')
+const app = express()
+const cors =require('cors')
+
+
+app.use(cors())
+
+app.get('/',(req,res)=> {
+    res.send(' INI GAME ZAPZING')
+})
+
+app.listen(3000,()=> {
+    console.log('INI GAME ZapZing http://localhost:3000');
+    
+})
