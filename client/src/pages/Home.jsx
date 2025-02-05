5a8a8d57f856870f03cfe00ca3ccaace81669131
zhafran2 <zhafranm.irsyad@gmail.com>
2025-02-05 15:54:12 +0700

    feat:halaman home done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,23 @@
+import ChatPage from "../components/Chat";
+
 export default function Home() {
-  return <></>;
+ 
+
+
+
+
+  return<>
+  <div className="flex h-screen">
+      {/* Main Content */}
+      <div className="flex-grow bg-white p-4">
+        <h1 className="text-2xl font-bold">Welcome to Home</h1>
+        <p>Main content goes here...</p>
+      </div>
+
+      {/* Sidebar Chat */}
+      <div className="w-80 bg-gray-100 shadow-lg border-l">
+        <ChatPage />
+      </div>
+    </div>
+  </>
 }
